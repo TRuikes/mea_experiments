d933ab316bf276e6a23118e7150cf55d0c5afef5
buisnesspersion <noneofyourbuisness@buisness.fr>
2026-04-23 17:14:49 +0200

    did more things - T

--- sonogenetics/analysis/lib/analysis_params.py
+++ sonogenetics/analysis/lib/analysis_params.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 # dataset_dir = Path(r'C:\thijs\sono_data\dataset')
-# dataset_dir = Path(r'E:\sono\dataset')
-dataset_dir = Path(r'C:\sono')
+dataset_dir = Path(r'E:\sono\dataset')
+# dataset_dir = Path(r'C:\sono')
 # figure_dir_analysis = Path(r'C:\thijs\sono_data\figures')
-# figure_dir_analysis = Path(r'E:\sono\figures')
-figure_dir_analysis = Path(r'C:\sono\figures')
+figure_dir_analysis = Path(r'E:\sono\figures')
+# figure_dir_analysis = Path(r'C:\sono\figures')
 
 
 data_list = [
@@ -12,10 +12,10 @@ data_list = [
     # '2026-02-16 mouse c57 566 eMSCL A',
     # '2026-02-19 mouse c57 5713 Mekano6 A',
     # '2026-03-25 mouse c57 617 Mekano6 B',
-    # '2026-04-08 mouse c57 621 Mekano4 A',
+    '2026-04-08 mouse c57 621 Mekano4 A',
     # '2026-04-08 mouse c57 621 Mekano4 B',
     # '2026-04-14 mouse c57 612 eMSCL A',
     # '2026-04-16 mouse c57 614 Mekano6 A',
     # '2026-04-16 mouse c57 614 Mekano6 B',
-    '2026-04-16 mouse c57 614 Mekano6 C',
+    # '2026-04-16 mouse c57 614 Mekano6 C',
 ]
