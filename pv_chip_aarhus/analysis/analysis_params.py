6f9d0ea98e72a82776564103cf298f0216913539
Asbjørn Cortnum Jørgensen <au617715@uni.au.dk>
2026-07-09 18:00:26 +0200

    Filepath changes and minimal intial analysis plots

--- pv_chip_aarhus/analysis/analysis_params.py
+++ pv_chip_aarhus/analysis/analysis_params.py
@@ -1,4 +1,9 @@
 from pathlib import Path
 
-dataset_dir = Path(r'E:\Denmark_idv\dataset')
-figure_dir_analysis = Path('E:\Denmark_idv\figures')
+dataset_dir = Path(
+    r"D:\ACJ\20260604\20260604 PV chip d-grease rd1 758 eye1 2nd half\Analysis\raw_filtered\dataset"
+)
+
+figure_dir_analysis = Path(
+    r"D:\ACJ\20260604\20260604 PV chip d-grease rd1 758 eye1 2nd half\Analysis\figures"
+)
