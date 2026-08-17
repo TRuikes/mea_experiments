896929ca0a1450c665e5569ba4a5ea0b847248d3
Asbjørn Cortnum Jørgensen <au617715@uni.au.dk>
2026-08-17 22:31:55 +0200

    Updated population analysis and more loaded sessions, but problems with other recordings than laser duration

--- pv_chip_aarhus/preprocessing/dataset_sessions.py
+++ pv_chip_aarhus/preprocessing/dataset_sessions.py
@@ -6,10 +6,15 @@ local_dir: path to local copy of raw data, for faster reading (can set to None)
 
 
 """
-
+# add the parent dir containing the rawfiles whereon you ran the clustering
 
 dataset_sessions = {
-    #'test_data': dict(raw_data_dir=r'D:\ACJ\20260604\20260604 PV chip d-grease rd1 758 eye1 2nd half\Analysis\raw_filtered'),  # add the parent dir containing the rawfiles whereon you ran the clustering
-    '20260601_eye1_1st': dict(raw_data_dir=r'D:\ACJ\20260601\20260601 PV chip rd1 770 eye1 1st half\Analysis\raw_filtered'),
+    
+    #'20260601_eye1_1st': dict(raw_data_dir=r'D:\ACJ\20260601\20260601 PV chip rd1 770 eye1 1st half\Analysis\raw_filtered'),
+    #'20260603_eye2_1st': dict(raw_data_dir=r'D:\ACJ\20260603\20260603 Fake chip rd1 766 eye2 1st half\Analysis\raw_filtered'), # PROBLEM for DEL2 which was only laser instead of both
+    '20260603_eye2_2nd': dict(raw_data_dir=r'D:\ACJ\20260603\20260603 Fake chip d-grease rd1 766 eye2 2nd half\Analysis\raw_filtered'),
+    #'20260604_eye1_2nd': dict(raw_data_dir=r'D:\ACJ\20260604\20260604 PV chip d-grease rd1 758 eye1 2nd half\Analysis\raw_filtered'),  
+    #'20260604_eye2_2nd': dict(raw_data_dir=r'D:\ACJ\20260604\20260604 Fake chip d-grease rd1 758 eye2 2nd half\Analysis\raw_filtered'),
+    #'20260527_eye2_1st': dict(raw_data_dir=r'D:\ACJ\20260527\20260527 No chip rd1 763 eye2 1st half\Analysis\raw_filtered'),   
         # algorithm. e.g.: D:/ACJ/20260604/20260604 PV chip d-grease rd1 758 eye1 2nd half/Analysis/raw_filtered/
 }
