f8aba74971b8260874c4387d026d856879f42fe2
Asbjørn Cortnum Jørgensen <au617715@uni.au.dk>
2026-08-20 02:25:22 +0200

    State of code at first draft for paper

--- pv_chip_aarhus/analysis/analysis_params.py
+++ pv_chip_aarhus/analysis/analysis_params.py
@@ -1,19 +1,23 @@
 from pathlib import Path
 
 dataset_dir = Path(
+    #r"D:\ACJ\20260527\20260527 No chip rd1 763 eye2 1st half\Analysis\raw_filtered\dataset"
     #r"D:\ACJ\20260601\20260601 PV chip rd1 770 eye1 1st half\Analysis\raw_filtered\dataset"
     #r"D:\ACJ\20260603\20260603 Fake chip rd1 766 eye2 1st half\Analysis\raw_filtered\dataset"
-    r"D:\ACJ\20260603\20260603 Fake chip d-grease rd1 766 eye2 2nd half\Analysis\raw_filtered\dataset"
+    #r"D:\ACJ\20260603\20260603 Fake chip d-grease rd1 766 eye2 2nd half\Analysis\raw_filtered\dataset"
     #r"D:\ACJ\20260604\20260604 PV chip d-grease rd1 758 eye1 2nd half\Analysis\raw_filtered\dataset"
     #r"D:\ACJ\20260604\20260604 Fake chip d-grease rd1 758 eye2 2nd half\Analysis\raw_filtered\dataset"
-    #r"D:\ACJ\20260527\20260527 No chip rd1 763 eye2 1st half\Analysis\raw_filtered\dataset"
+    r"D:\ACJ\20260605\20260605 PV chip rd1 772 eye1 2nd half\Analysis\raw_filtered\dataset"
+    
 )
 
 figure_dir_analysis = Path(
+    #r"D:\ACJ\20260527\20260527 No chip rd1 763 eye2 1st half\Analysis\figures"
     #r"D:\ACJ\20260601\20260601 PV chip rd1 770 eye1 1st half\Analysis\figures"
     #r"D:\ACJ\20260603\20260603 Fake chip rd1 766 eye2 1st half\Analysis\figures"
-    r"D:\ACJ\20260603\20260603 Fake chip d-grease rd1 766 eye2 2nd half\Analysis\figures"
+    #r"D:\ACJ\20260603\20260603 Fake chip d-grease rd1 766 eye2 2nd half\Analysis\figures"
     #r"D:\ACJ\20260604\20260604 PV chip d-grease rd1 758 eye1 2nd half\Analysis\figures"
     #r"D:\ACJ\20260604\20260604 Fake chip d-grease rd1 758 eye2 2nd half\Analysis\figures"
-    #r"D:\ACJ\20260527\20260527 No chip rd1 763 eye2 1st half\Analysis\figures"
+    r"D:\ACJ\20260605\20260605 PV chip rd1 772 eye1 2nd half\Analysis\figures"
+    
 )
--- pv_chip_aarhus/preprocessing/params.py
+++ pv_chip_aarhus/preprocessing/params.py
@@ -1,12 +1,13 @@
 # dataset_dir = r'E:\Denmark_idv'  # PC thijs
 #dataset_dir = r'C:\thijs\asbjorn'  # PC IDV
 
+#dataset_dir = r'D:\ACJ\20260527\20260527 No chip rd1 763 eye2 1st half\Analysis\raw_filtered'
 #dataset_dir = r'D:\ACJ\20260601\20260601 PV chip rd1 770 eye1 1st half\Analysis\raw_filtered'
 #dataset_dir = r'D:\ACJ\20260603\20260603 Fake chip rd1 766 eye2 1st half\Analysis\raw_filtered'
-dataset_dir = r'D:\ACJ\20260603\20260603 Fake chip d-grease rd1 766 eye2 2nd half\Analysis\raw_filtered'
+#dataset_dir = r'D:\ACJ\20260603\20260603 Fake chip d-grease rd1 766 eye2 2nd half\Analysis\raw_filtered'
 #dataset_dir = r'D:\ACJ\20260604\20260604 PV chip d-grease rd1 758 eye1 2nd half\Analysis\raw_filtered'
 #dataset_dir = r'D:\ACJ\20260604\20260604 Fake chip d-grease rd1 758 eye2 2nd half\Analysis\raw_filtered' #PC AU
-#dataset_dir = r'D:\ACJ\20260527\20260527 No chip rd1 763 eye2 1st half\Analysis\raw_filtered'
+dataset_dir = r'D:\ACJ\20260605\20260605 PV chip rd1 772 eye1 2nd half\Analysis\raw_filtered'
 
 data_sample_rate = 2e4
 data_type = 'uint16'
